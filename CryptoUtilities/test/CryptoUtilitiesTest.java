8987d1341a914ab869e87f5e6eae2d95f603085d
Nabeu Habetaslassa <143747400+nabeuH@users.noreply.github.com>
2024-12-18 14:28:50 -0500

    Add files via upload

---
+++ CryptoUtilities/test/CryptoUtilitiesTest.java
@@ -0,0 +1,131 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import components.naturalnumber.NaturalNumber;
+import components.naturalnumber.NaturalNumber2;
+
+/**
+ * @author Nabeu Habetaslassa
+ *
+ */
+public class CryptoUtilitiesTest {
+
+    /*
+     * Tests of reduceToGCD
+     */
+
+    @Test
+    public void testReduceToGCD_0_0() {
+        NaturalNumber n = new NaturalNumber2(0);
+        NaturalNumber nExpected = new NaturalNumber2(0);
+        NaturalNumber m = new NaturalNumber2(0);
+        NaturalNumber mExpected = new NaturalNumber2(0);
+        CryptoUtilities.reduceToGCD(n, m);
+        assertEquals(nExpected, n);
+        assertEquals(mExpected, m);
+    }
+
+    @Test
+    public void testReduceToGCD_30_21() {
+        NaturalNumber n = new NaturalNumber2(30);
+        NaturalNumber nExpected = new NaturalNumber2(3);
+        NaturalNumber m = new NaturalNumber2(21);
+        NaturalNumber mExpected = new NaturalNumber2(0);
+        CryptoUtilities.reduceToGCD(n, m);
+        assertEquals(nExpected, n);
+        assertEquals(mExpected, m);
+    }
+
+    /*
+     * Tests of isEven
+     */
+
+    @Test
+    public void testIsEven_0() {
+        NaturalNumber n = new NaturalNumber2(0);
+        NaturalNumber nExpected = new NaturalNumber2(0);
+        boolean result = CryptoUtilities.isEven(n);
+        assertEquals(nExpected, n);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void testIsEven_1() {
+        NaturalNumber n = new NaturalNumber2(1);
+        NaturalNumber nExpected = new NaturalNumber2(1);
+        boolean result = CryptoUtilities.isEven(n);
+        assertEquals(nExpected, n);
+        assertEquals(false, result);
+    }
+
+    /*
+     * Tests of powerMod
+     */
+
+    @Test
+    public void testPowerMod_0_0_2() {
+        NaturalNumber n = new NaturalNumber2(0);
+        NaturalNumber nExpected = new NaturalNumber2(1);
+        NaturalNumber p = new NaturalNumber2(0);
+        NaturalNumber pExpected = new NaturalNumber2(0);
+        NaturalNumber m = new NaturalNumber2(2);
+        NaturalNumber mExpected = new NaturalNumber2(2);
+        CryptoUtilities.powerMod(n, p, m);
+        assertEquals(nExpected, n);
+        assertEquals(pExpected, p);
+        assertEquals(mExpected, m);
+    }
+
+    @Test
+    public void testPowerMod_17_18_19() {
+        NaturalNumber n = new NaturalNumber2(17);
+        NaturalNumber nExpected = new NaturalNumber2(1);
+        NaturalNumber p = new NaturalNumber2(18);
+        NaturalNumber pExpected = new NaturalNumber2(18);
+        NaturalNumber m = new NaturalNumber2(19);
+        NaturalNumber mExpected = new NaturalNumber2(19);
+        CryptoUtilities.powerMod(n, p, m);
+        assertEquals(nExpected, n);
+        assertEquals(pExpected, p);
+        assertEquals(mExpected, m);
+    }
+
+    @Test
+    public void testReduceToGCD_10_5() {
+        NaturalNumber n = new NaturalNumber2(10);
+        NaturalNumber m = new NaturalNumber2(5);
+        CryptoUtilities.reduceToGCD(n, m);
+        assertEquals(n.toString(), "5");
+        assertEquals(m.toString(), "0");
+    }
+
+    public void testReduceToGCD_50_20() {
+        NaturalNumber n = new NaturalNumber2(50);
+        NaturalNumber m = new NaturalNumber2(20);
+        CryptoUtilities.reduceToGCD(n, m);
+        assertEquals(n.toString(), "10");
+        assertEquals(m.toString(), "0");
+    }
+
+    public void testReduceToGCD_20_40() {
+        NaturalNumber n = new NaturalNumber2(20);
+        NaturalNumber m = new NaturalNumber2(40);
+        CryptoUtilities.reduceToGCD(n, m);
+        assertEquals(n.toString(), "20");
+        assertEquals(m.toString(), "0");
+    }
+
+    @Test
+    public void testIsEven_10() {
+        NaturalNumber n = new NaturalNumber2(10);
+        assertEquals(CryptoUtilities.isEven(n), true);
+    }
+
+    @Test
+    public void testIsEven_9() {
+        NaturalNumber n = new NaturalNumber2(9);
+        assertEquals(CryptoUtilities.isEven(n), false);
+    }
+
+}
